b8197698e80c6ba2eb243064c04f6be54f383317
Conor Hollern <Conor@Conors-MacBook-Pro.local>
2018-01-24 18:49:07 -0500

    Added routing and controllers

---
+++ Controllers/AuthorsControllers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+
+	public class AuthorsControllers : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return Content("this is the Authors Controller's Index action!");
+		}
+
+		public IActionResult History()
+		{
+			return Content("this is the Authors Controller's Featured action!");
+		}
+	}
+}
---
+++ Controllers/BooksControllers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+
+	public class BooksController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return Content("this is the Books Controller's Index action!");
+		}
+
+		public IActionResult New()
+		{
+			return Content("this is the Books Controller's New action!");
+		}
+	}
+}
